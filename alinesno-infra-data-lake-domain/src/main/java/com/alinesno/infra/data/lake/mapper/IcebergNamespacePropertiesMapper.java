5c9d367edf60d6bbadabb3f7e22f71506a6dc4bd
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:19 +0800

    feat:创建IcebergNamespacePropertiesMapper接口，继承BaseMapper

---
+++ alinesno-infra-data-lake-domain/src/main/java/com/alinesno/infra/data/lake/mapper/IcebergNamespacePropertiesMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.lake.mapper;
+
+import com.alinesno.infra.data.lake.entity.IcebergNamespacePropertiesEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface IcebergNamespacePropertiesMapper extends BaseMapper<IcebergNamespacePropertiesEntity> {
+
+}
