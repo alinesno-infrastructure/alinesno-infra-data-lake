1c541e86fa889d780e8c44e3d34befe4567ca67b
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:20 +0800

    feat:创建CatalogDto类，包含名称、类型、注释和属性字段

---
+++ alinesno-infra-data-lake-rest/src/main/java/com/alinesno/infra/data/lake/rest/dto/CatalogDto.java
@@ -0,0 +1,19 @@
+//package com.alinesno.infra.data.lake.rest.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.Map;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CatalogDto {
+//    private String name;
+//    private String type;
+//    private String comment;
+//    private Map<String, String> properties;
+//}
