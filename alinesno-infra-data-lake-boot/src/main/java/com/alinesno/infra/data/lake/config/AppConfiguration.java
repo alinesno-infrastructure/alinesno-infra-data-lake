9d80db4b218617ae72bf222fc98ca42dec6afd47
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:20 +0800

    feat:创建AppConfiguration配置类，集成数据权限、日志切面等功能。

---
+++ alinesno-infra-data-lake-boot/src/main/java/com/alinesno/infra/data/lake/config/AppConfiguration.java
@@ -0,0 +1,40 @@
+package com.alinesno.infra.data.lake.config;
+
+import com.alinesno.infra.common.extend.datasource.enable.EnableInfraDataScope;
+import com.alinesno.infra.common.facade.enable.EnableActable;
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
+import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.dtflys.forest.springboot.annotation.ForestScan;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.x.file.storage.spring.EnableFileStorage;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类
+ */
+@Slf4j
+@EnableFileStorage
+@EnableInfraDataScope
+@EnableActable
+@EnableInfraSsoApi
+@MapperScan("com.alinesno.infra.data.**.mapper")
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer"
+})
+@Configuration
+public class AppConfiguration implements CommandLineRunner  {
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("AppConfiguration.run");
+    }
+
+    @Bean
+    public LogAspect getLogAspect(){
+        return new LogAspect() ;
+    }
+
+}
