2192489b911618887540724fa508ce4e54c5b38a
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:18 +0800

    feat:创建下载记录服务接口DownloadRecordService

---
+++ alinesno-infra-data-lake-storage/src/main/java/com/alinesno/infra/data/lake/storage/service/DownloadRecordService.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.data.lake.storage.service;
+
+import com.alinesno.infra.data.lake.storage.entity.DownloadRecordEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface DownloadRecordService extends IService<DownloadRecordEntity> {
+    // 可扩展自定义查询/统计方法
+}
