785b00c2bb17816eceebd371de702b89f61e112e
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:17 +0800

    feat:创建CatalogProperties类，用于存储目录属性信息。

---
+++ alinesno-infra-data-lake-rest/src/main/java/com/alinesno/infra/data/lake/rest/dto/CatalogProperties.java
@@ -0,0 +1,16 @@
+//package com.alinesno.infra.data.lake.rest.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.Map;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CatalogProperties {
+//    private Map<String, String> props;
+//}
