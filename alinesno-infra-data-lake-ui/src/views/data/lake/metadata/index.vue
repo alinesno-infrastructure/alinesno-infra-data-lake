89fb843321b8cbe3e4d6e1d1ab1c86d213c39074
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:20 +0800

    feat:创建元数据管理页面，包含侧边栏和主面板组件

---
+++ alinesno-infra-data-lake-ui/src/views/data/lake/metadata/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <!-- 类型数据 -->
+      <el-col :span="5" :xs="24">
+        <!-- 添加node-click事件监听 -->
+        <MetaSidebarPanel 
+          ref="metaSidebarPanelRef" 
+          @node-click="handleSidebarNodeClick"
+        />
+      </el-col>
+
+      <!-- 元数据信息 -->
+      <el-col :span="19" :xs="20">    
+        <MetaMainPanel 
+          @refreshSidebar="refreshSidebar"
+          ref="metaMainPanelRef" 
+          :current-node="currentNode"
+        />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+
+import MetaSidebarPanel from "./sidebar"
+import MetaMainPanel from "./metaPanel"
+import { ref } from 'vue';
+
+const metaSidebarPanelRef = ref(null) 
+const metaMainPanelRef = ref(null) 
+const currentNode = ref(null);  // 存储当前选中的节点
+
+// 处理侧边栏节点点击事件
+const handleSidebarNodeClick = (node) => {
+  currentNode.value = node;  // 更新当前节点
+  // 通知metaMainPanel刷新数据
+  if (metaMainPanelRef.value) {
+    metaMainPanelRef.value.loadNodeDetails(node);
+  }
+};
+
+const refreshSidebar = () => {
+  metaSidebarPanelRef.value.loadCatalogData();
+};
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  background-color: #fff;
+  padding: 10px;
+}
+</style>
