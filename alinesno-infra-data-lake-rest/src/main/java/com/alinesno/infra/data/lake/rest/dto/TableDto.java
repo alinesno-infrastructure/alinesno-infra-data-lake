c0c3096e01c65f569447ccc786a89ba336e290e6
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:19 +0800

    feat:创建TableDto类，包含表的基本信息和属性定义。

---
+++ alinesno-infra-data-lake-rest/src/main/java/com/alinesno/infra/data/lake/rest/dto/TableDto.java
@@ -0,0 +1,23 @@
+//package com.alinesno.infra.data.lake.rest.dto;
+//
+//import com.alinesno.infra.data.lake.rest.dto.TableSchemaDto;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.Map;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class TableDto {
+//    private String catalogName;
+//    private String databaseName;
+//    private String tableName;
+//    private TableSchemaDto schema;
+//    private Map<String, String> properties;
+//    private String comment;
+//    private long createTime;
+//}
