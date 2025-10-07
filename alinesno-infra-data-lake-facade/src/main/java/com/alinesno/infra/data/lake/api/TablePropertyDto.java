345fb2ea06d8f46d9390a1bffcf665eb9cdc4779
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:18 +0800

    feat:创建TablePropertyDto类，用于封装表属性信息

---
+++ alinesno-infra-data-lake-facade/src/main/java/com/alinesno/infra/data/lake/api/TablePropertyDto.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.data.lake.api;
+
+import lombok.Data;
+
+/**
+ * 表属性DTO
+ */
+@Data
+public class TablePropertyDto {
+
+    private String key;
+
+    private String value;
+}
