e972e673cc390843ee71f707ba44a9aaee54574f
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:19 +0800

    feat:创建CreateCatalogRequest类，定义目录请求参数结构

---
+++ alinesno-infra-data-lake-rest/src/main/java/com/alinesno/infra/data/lake/rest/dto/CreateCatalogRequest.java
@@ -0,0 +1,22 @@
+//package com.alinesno.infra.data.lake.rest.dto;
+//
+//import jakarta.validation.constraints.NotBlank;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.Map;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CreateCatalogRequest {
+//    @NotBlank
+//    private String name;
+//    @NotBlank
+//    private String type;
+//    private String comment;
+//    private Map<String, String> properties;
+//}
