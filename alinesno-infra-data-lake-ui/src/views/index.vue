dcb6276dfc9b7eb8fda1c771b771f8656aa11b18
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:19 +0800

    feat: 创建首页页面组件，包含应用信息展示和功能模块引入。

---
+++ alinesno-infra-data-lake-ui/src/views/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="app-container acp-dashboard">
+
+    <el-row>
+      <el-col :span="24">
+        <div class="aip-appinfo-header">
+          <div class="header-icon-banner">
+            <i class="fa-brands fa-hornbill"></i>
+          </div>
+          <div class="icon">
+            <div class="title">
+              {{ currentEnvClusterObj.appName}}
+            </div>
+            <div class="title-desc">
+                  {{ currentEnvClusterObj.clusterName }}
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+
+    <!-- 应用套件和状态 -->
+    <OperationWorkspaceService />
+
+    <!-- 运行应用列表 -->
+    <OperationWorkspaceApps />
+
+  </div>
+</template>
+
+<script setup name="Index">
+
+import OperationWorkspaceApps from './operation-workspace/apps.vue'
+import OperationWorkspaceService from './operation-workspace/service.vue'
+
+const currentEnvClusterObj = ref({
+  appName: '轻量级数据湖服务',
+  clusterName: '轻量级数据湖服务，提供低成本、易运维的数据集中存储与管理，支持多源数据接入与无缝分析，简化大数据技术栈。'
+})
+
+</script>
+
+
+<style lang="scss" scoped>
+
+.acp-dashboard {
+  height: calc(100vh - 40px);
+}
+
+.aip-appinfo-header{
+  position: relative;
+  padding: 20px;
+  overflow: hidden;
+
+  .header-icon-banner {
+    float: left;
+    font-size: 2.4rem;
+    margin-right: 10px;
+    color: #3b5998;
+  }
+
+  .head-app-status{
+    float: right;
+    font-size: 14px;
+    line-height: 1.7rem;
+    margin-bottom: 10px;
+
+    .integrated-item-box {
+
+      margin-left: 10px;
+      float: right;
+
+      ul , li {
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+      }
+      li {
+        float:right ;
+        margin-right: 10px;
+
+        img {
+          border-radius: 5px;
+          width: 25px;
+        }
+      }
+
+    }
+  }
+
+  .cluster-info {
+    float: right;
+    position: relative;
+    font-size: 14px;
+    margin-left: 10px;
+    font-weight: bold;
+    margin-top: 10px;
+    color: #3b5998;
+
+    span {
+      margin-left: 20px;
+    }
+  }
+
+  .icon {
+    float: left;
+  }
+
+  .title {
+    display: block;
+    font-weight: 600;
+    font-style: normal;
+    font-size: 24px;
+    color: #242e42;
+    text-shadow: 0 4px 8px rgba(36, 46, 66, 0.1);
+    margin-bottom: 10px;
+  }
+
+  .title-desc {
+    color: #79879c !important;
+    font-size: 12px;
+  }
+}
+</style>
