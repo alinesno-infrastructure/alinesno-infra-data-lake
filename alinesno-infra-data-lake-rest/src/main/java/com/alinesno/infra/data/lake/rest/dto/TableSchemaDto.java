7fe5f24b8915d47b1554a998c3eb48f0965c5bf2
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:17 +0800

    feat:创建TableSchemaDto类，定义表结构相关字段和属性

---
+++ alinesno-infra-data-lake-rest/src/main/java/com/alinesno/infra/data/lake/rest/dto/TableSchemaDto.java
@@ -0,0 +1,18 @@
+//package com.alinesno.infra.data.lake.rest.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.List;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class TableSchemaDto {
+//    private List<FieldDto> fields;
+//    private List<String> partitionKeys;
+//    private String primaryKey; // simple example; 可扩展成 List<String>
+//}
