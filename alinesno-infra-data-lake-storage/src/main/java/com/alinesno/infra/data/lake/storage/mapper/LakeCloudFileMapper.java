d409d68bdac783b1165693688c0de7742a063337
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:20 +0800

    feat:创建LakeCloudFileMapper接口，用于湖存储非结构化文件操作

---
+++ alinesno-infra-data-lake-storage/src/main/java/com/alinesno/infra/data/lake/storage/mapper/LakeCloudFileMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.data.lake.storage.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.lake.storage.entity.LakeCloudFileEntity;
+
+/**
+ * 湖存储非结构化文件Mapper接口
+ */
+public interface LakeCloudFileMapper extends IBaseMapper<LakeCloudFileEntity> {
+}
