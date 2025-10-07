43ba3ebf88ea722040a5a158ef54d4158bf7b653
李娜(Allen) <allenAip@gmail.com>
2025-10-07 21:56:20 +0800

    feat:创建数据库请求参数类CreateDatabaseRequest

---
+++ alinesno-infra-data-lake-rest/src/main/java/com/alinesno/infra/data/lake/rest/dto/CreateDatabaseRequest.java
@@ -0,0 +1,18 @@
+//package com.alinesno.infra.data.lake.rest.dto;
+//
+//import jakarta.validation.constraints.NotBlank;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CreateDatabaseRequest {
+//    @NotBlank
+//    private String name;
+//
+//    private String comment;
+//}
