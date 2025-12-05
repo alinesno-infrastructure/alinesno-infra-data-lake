a30ccb1c07a53abad82d48c5629f33c4ce5d1b1f
刘芳(Luna) <lunaAip@gmail.com>
2025-12-06 00:23:41 +0800

    feat: 添加DownloadTokenMapper接口，定义下载令牌操作方法

--- alinesno-infra-data-lake-storage/src/main/java/com/alinesno/infra/data/lake/storage/mapper/DownloadTokenMapper.java
+++ alinesno-infra-data-lake-storage/src/main/java/com/alinesno/infra/data/lake/storage/mapper/DownloadTokenMapper.java
@@ -1,9 +1,7 @@
 package com.alinesno.infra.data.lake.storage.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.data.lake.storage.entity.DownloadRecordEntity;
 import com.alinesno.infra.data.lake.storage.entity.DownloadTokenEntity;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
